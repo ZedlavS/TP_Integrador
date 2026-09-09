f56d51c6c7ddbfd14df9eeb320702b0d9f03c457
ZedlavS <12valdezsantiago@gmail.com>
2026-09-09 19:46:40 -0300

    no lo subi bien lol

--- Domain/poder.cs
+++ Domain/poder.cs
@@ -2,10 +2,8 @@ public class Poder
 {
     public nivelPoder Nivel { get; set; }
     public string Tipo { get; set; }
-    public string Origen { get; set; }
-    public string Limitaciones { get; set; }
-    public alcancePoder Alcance { get; set; }
-    public accesorioPoder Accesorio { get; set; }
+
+    public string nombre { get; set; }
 
 }
 
@@ -14,19 +12,4 @@ public enum nivelPoder
     Bajo,
     Medio,
     Alto
-}
-
-public enum alcancePoder
-{
-    Personal,
-    Cercano,
-    Lejano
-}
-
-public enum accesorioPoder
-{
-    Ninguno,
-    Amuleto,
-    Arma,
-    Vestimenta
 }
