5df299ddf1d2dea5d49140633f12682bdbd1ba1f
atiracJ <caritamanuel87@gmail.com>
2026-09-09 22:55:48 -0300

    hice la clase Central

---
+++ Domain/Central.cs
@@ -0,0 +1,13 @@
+public class Central
+{
+
+    public void PriorizarPorZona(Zona zona)
+    {
+        
+    }
+
+    public void AsignarEquipo(incidente incidenteActual, Equipo equipoAsignado)
+    {
+        
+    }
+}
